d64f5b2cd90cf7f6beef25d999916dd62916cb38
djemoui <34734830+badrdjemoui@users.noreply.github.com>
2024-08-19 18:38:11 +0200

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,18 +1,159 @@
 import 'package:flutter/material.dart';
+import 'sqldb.dart';
 
-void main() {
-  runApp( MyApp());
-}
-class MyApp extends StatelessWidget{
+
+void main() => runApp( RunMyApp());
+
+class RunMyApp extends StatelessWidget {
+   RunMyApp({super.key});
+
+  SqlDb db = new SqlDb();
+
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
+    return  MaterialApp(
+      title: 'Retrieve Text Input',
+      theme: ThemeData(primarySwatch: Colors.green),
+      debugShowCheckedModeBanner: false,
+      home: MyCustomForm(),
+    );
+  }
+}
 
-                       home:Text("djemoui badr")
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class. This class holds the data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value of the TextField.
+  final myController1 = TextEditingController();
+  final myController2 = TextEditingController();
+  final myController3 = TextEditingController();
+  final myController4 = TextEditingController();
+  final myController5 = TextEditingController();
+       double cost =0.0;
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController1.dispose();
+    myController2.dispose();
+    myController3.dispose();
+    myController4.dispose();
+    myController4.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+                           appBar:
+                               AppBar(title: const Center(
+                                 child: Text(" الدجاج المذبوح",style:TextStyle(
+                                   // color:Color.fromRGBO(20, 200, 240, 20),
+                                    color: Colors.blueAccent,
+                                       //   color: Color(0xff3400db),
+                                   fontSize: 22.2,
+                                   fontWeight: FontWeight.bold,
+                                     backgroundColor: Colors.white60),
+                                        ))),
+                           body:
+                           /*const Text("djemoui badr",style:TextStyle(
+                                     // color:Color.fromRGBO(20, 200, 240, 20),
+                                      color: Colors.greenAccent,
+                                     //   color: Color(0xff3400db),
+                                        fontSize: 60.2,
+                                    fontWeight: FontWeight.bold,
+                                    backgroundColor: Colors.amberAccent,
+
+                                                                        )
+                                         ),
+    */
+  Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+             children: <Widget>[
+
+               TextFormField(
+                   controller: myController1,
+                   decoration: const InputDecoration(
+                     border: UnderlineInputBorder(),
+                     labelText: ' اسم الزبون ',
+                   )
+               ),
+
+               TextFormField(
+                   controller: myController2,
+                   decoration: const InputDecoration(
+                     border: UnderlineInputBorder(),
+                     labelText: ' التاريخ ',
+                   )
+               ),
+
+   TextFormField(
+       controller: myController3,
+   keyboardType: TextInputType.number,
+         decoration: const InputDecoration(
+           border: UnderlineInputBorder(),
+            labelText: ' العدد ',
+
+           )
+         ),
+
+    TextFormField(
+        controller: myController4,
+        keyboardType: TextInputType.number,
+    decoration: const InputDecoration(
+    border: UnderlineInputBorder(),
+    labelText: 'الوزن الكلي',
+    )
+    ),
+    TextFormField(
+        controller: myController5,
+        keyboardType: TextInputType.number,
+    decoration: const InputDecoration(
+    border: UnderlineInputBorder(),
+    labelText: 'وزن الكلغ ',
+    )
+    ),
+               MaterialButton(
+
+                              onPressed:()
+                                     {
+cost = double.parse(myController4.text)*double.parse(myController5.text);
+                             showDialog(
+                                              context: context,
+                                             builder: (context) {
+                                                         return AlertDialog(
+                       // Retrieve the text that user has entered by using the
+                       // TextEditingController.
+                                                            content: Text(cost.toString()),
+                                                                     );
+                                     },
+                             );
+
+
+               },
+               child: Text('حساب '),
+                 color:Color(0xFFB9F6CA),
+                 height: 30,
+                 textColor:Color(0xFFFF6F00),
+                 //shape: ,
+
+               )
+    ],
+    ) ,
+
 
                     );
 
   }
+
+
 
   
 }
---
+++ sqldb.dart
@@ -0,0 +1,75 @@
+import 'dart:ui';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class SqlDb {
+  static Database? _db;
+  Future<Database?> get db async {
+
+    if (_db==null) {
+                    _db =await initialDb();
+                    return _db;
+                   }else
+                     {
+                        return _db;
+                     }
+  }
+  initialDb() async {
+String dbpath = await getDatabasesPath();
+String path =join(dbpath,'checken.db');
+Database mydb =await openDatabase(path,onCreate:_onCreate,version: 1,onUpgrade: _onUpgrade);
+return mydb;
+}
+
+  _onUpgrade(Database db , int oldversion , int newversion ) {
+
+
+    print("onUpgrade =====================================") ;
+
+  }
+
+_onCreate(Database db ,int v) async{
+    await db.execute('''
+    create table "sales" (
+    id INTEGER AUTOINCREMENT NOT NULL PRIMARY KEY,
+     txt TEXT NOT NULL ,
+     ) 
+   
+     
+     
+     
+                    ''');
+    //  date TEXT,
+    //  nom TEXT,
+    //  nbr  INTEGER,
+    //  poid real,
+    //  unkg real,
+    //   versem real,
+    //   reste real,
+
+    print('create db and table');
+
+}
+readData(String sql) async {
+    Database? mydb = await db;
+    List<Map> response = await mydb!.rawQuery(sql);
+    return response;
+}
+  insertData(String sql) async {
+    Database? mydb = await db;
+    int response = await mydb!.rawInsert(sql);
+    return response;
+  }
+  updateData(String sql) async {
+    Database? mydb = await db;
+    int response = await mydb!.rawUpdate(sql);
+    return response;
+  }
+ deleteData(String sql) async {
+    Database? mydb = await db;
+    int response = await mydb!.rawDelete(sql);
+    return response;
+  }
+
+}
